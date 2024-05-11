229a0984993592f2d99d7016acf3749b42974a46
Grockkk <dela.kontakt@gmail.com>
2024-05-11 17:31:56 +0200

    migration operational

--- Kompik.Api/Entities/ProductCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kompik.Api.Entities
-{
-    public class ProductCategory
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string IconCSS { get; set; }
-    }
-}
---
+++ Kompik24.Api/Data/Kompik24DbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Data
+{
+    public class Kompik24DbContext:DbContext
+    {
+        public Kompik24DbContext(DbContextOptions<Kompik24DbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+			//Products
+			//Beauty Category
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 4,
+				Name = "Shoes",
+				IconCSS = "fas fa-shoe-prints"
+			});
+
+
+		}
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
